f4936ff5ac214642d96d9d92e866dcc2b631619d
Eduardo Viana <eduardocosta_viana@live.com>
2020-02-20 08:54:33 -0300

    Configurações Iniciais

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import routes from './routes';
+import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(cors());
+    this.server.use(express.json());
+    this.server.use(express.urlencoded({ extended: false }));
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
---
+++ src/database/index.js
@@ -0,0 +1,29 @@
+/* eslint-disable arrow-parens */
+import Sequelize from 'sequelize';
+
+import databaseConfig from './..config/database';
+
+const models = [];
+
+class Database {
+  constructor() {
+    this.connection = new Sequelize(databaseConfig);
+
+    this.init();
+    this.associate();
+  }
+
+  init() {
+    models.forEach(model => model.init(this.connection));
+  }
+
+  associate() {
+    models.forEach(model => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
+  }
+}
+
+export default new Database();
